de4ae0eddf3a1e7877f04fc79ea21ca5b8c7441e
Aaron A. King <kingaa@umich.edu>
2009-05-07 14:19:33 +0000

    all error checking is now done in the C code
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/subplex@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/subplex.R
+++ pkg/R/subplex.R
@@ -1,5 +1,4 @@
 subplex <- function (par, fn, tol = .Machine$double.eps, maxnfe = 10000, scale = 1, ...) {
-  if (!is.function(fn)) stop("'fn' must be a function")
   .Call("call_subplex",
         par,
         fn,
