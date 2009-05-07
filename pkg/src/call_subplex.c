46097fa2230873b6164cc579a81330f3ceae6f08
Aaron A. King <kingaa@umich.edu>
2009-05-07 14:16:44 +0000

    bug fix fix (?) memory allocation bug change the handling of global variables R_fcall is global and set once names for parameter vectors are set just once
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/subplex@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/call_subplex.c
+++ pkg/src/call_subplex.c
@@ -2,27 +2,23 @@
 #include <Rmath.h>
 #include <Rdefines.h>
 
-static SEXP subplex_obj_fn;
-static SEXP subplex_X_names;
-static SEXP subplex_envir;
+SEXP Xvec;
+SEXP subplex_envir;
+SEXP R_fcall;
 
-void F77_NAME(subplx) (double (*)(int *, double*), 
-		       int *, double *, int *, int *, 
-		       double *, double *, double *, 
-		       int *, double *, int *, int *);
+void F77_NAME(subplx) (double (*f)(int *n, double *x), int *n, double *tol, int *maxnfe, int *mode,
+		       double *scale, double *x, double *fx, int *nfe, double *work, int *iwork, 
+		       int *iflag);
 
-
-static double subplex_user_fn (int *n, double *x) 
+static double subplex_objective (int *n, double *x) 
 {
   int nprotect = 0;
   double *xp, retval;
   int k;
-  SEXP R_fcall, X, ans;
-  PROTECT(X = NEW_NUMERIC(*n)); nprotect++;
-  xp = REAL(X);
+  SEXP ans;
+  R_CheckUserInterrupt();
+  xp = REAL(Xvec);
   for (k = 0; k < *n; k++) xp[k] = x[k];
-  SET_NAMES(X, subplex_X_names);
-  PROTECT(R_fcall = lang2(subplex_obj_fn,X)); nprotect++;
   PROTECT(ans = eval(R_fcall,subplex_envir)); nprotect++;
   retval = REAL(ans)[0];
   UNPROTECT(nprotect);
@@ -32,23 +28,27 @@ static double subplex_user_fn (int *n, double *x)
 SEXP call_subplex (SEXP x, SEXP f, SEXP tol, SEXP maxnfe, SEXP scale, SEXP rho) 
 {
   int nprotect = 0;
-  double *work, *scalp, *xp, *Xp, fx;
-  int n, *iwork, mode, nfe, iflag = 0;
+  double *work, *scalp, *xp, *Xp;
+  int n, *iwork, mode = 0;
   int k, nscal;
-  SEXP ans, ansnames, X, val, counts, conv;
+  SEXP ans, ansnames, X, Xnames, val, counts, conv, fn;
 
   n = GET_LENGTH(x);
   PROTECT(subplex_envir=rho); nprotect++;
-  PROTECT(subplex_obj_fn=f); nprotect++;
-  PROTECT(subplex_X_names=GET_NAMES(x)); nprotect++;
-  PROTECT(X = NEW_NUMERIC(n)); nprotect++;
-  SET_NAMES(X,subplex_X_names);
+  PROTECT(fn=f); nprotect++;
+  PROTECT(Xnames=GET_NAMES(x)); nprotect++;
+  PROTECT(X = NEW_NUMERIC(n)); nprotect++; // for returning
+  PROTECT(Xvec = NEW_NUMERIC(n)); nprotect++; // for internal use within subplex_objective
+  SET_NAMES(X,Xnames);
+  SET_NAMES(Xvec,Xnames);
+  PROTECT(R_fcall = lang2(fn,Xvec)); nprotect++;
 
   xp = REAL(x); Xp = REAL(X);
   for (k = 0; k < n; k++) Xp[k] = xp[k];
 
-  work = (double *) Calloc(2*n+46,double *);
-  iwork = (int *) Calloc(2*n,int *);
+  // the following memory allocation is based on an interpretation of the subplex documentation
+  work = (double *) R_alloc(n*(n+6)+1,sizeof(double));
+  iwork = (int *) R_alloc(2*n,sizeof(int));
 
   nscal = GET_LENGTH(scale);
   scalp = REAL(scale);
@@ -58,47 +58,24 @@ SEXP call_subplex (SEXP x, SEXP f, SEXP tol, SEXP maxnfe, SEXP scale, SEXP rho)
     for (k = 0; k < nscal; k++) scalp[k] = fabs(scalp[k]);
   }
   
-  mode = 0;
-  nfe = 0;
-  fx = 0;
-  F77_CALL(subplx)(subplex_user_fn,&n,REAL(tol),INTEGER(maxnfe),&mode,scalp,Xp,&fx,&nfe,work,iwork,&iflag);
-
-/*     call subplx (UserFun,n,tol,maxnfe,mode,scale,x,fx,nfe,work,iwork,iflag) */
-
   PROTECT(val = NEW_NUMERIC(1)); nprotect++;
-  REAL(val)[0] = fx;
   PROTECT(counts = NEW_INTEGER(1)); nprotect++;
-  INTEGER(counts)[0] = nfe;
   PROTECT(conv = NEW_INTEGER(1)); nprotect++;
-  INTEGER(conv)[0] = iflag;
-  PROTECT(ans = NEW_LIST(4)); nprotect++;
+
+  F77_CALL(subplx)(subplex_objective,&n,REAL(tol),INTEGER(maxnfe),&mode,scalp,Xp,REAL(val),INTEGER(counts),
+		   work,iwork,INTEGER(conv));
+
   PROTECT(ansnames = NEW_CHARACTER(4)); nprotect++;
   SET_STRING_ELT(ansnames,0,mkChar("par"));
   SET_STRING_ELT(ansnames,1,mkChar("value"));
   SET_STRING_ELT(ansnames,2,mkChar("counts"));
   SET_STRING_ELT(ansnames,3,mkChar("convergence"));
+  PROTECT(ans = NEW_LIST(4)); nprotect++;
   SET_NAMES(ans,ansnames);
   SET_VECTOR_ELT(ans,0,X);
   SET_VECTOR_ELT(ans,1,val);
   SET_VECTOR_ELT(ans,2,counts);
   SET_VECTOR_ELT(ans,3,conv);
-
-/*   if (inherits(func, "NativeSymbol"))  */
-/*     { */
-/*       derivs = (deriv_func *) R_ExternalPtrAddr(func); */
-/*       /\* If there is an initializer, use it here *\/ */
-/*       if (!isNull(initfunc)) */
-/* 	{ */
-/* 	  initializer = (init_func *) R_ExternalPtrAddr(initfunc); */
-/* 	  initializer(Initodeparms); */
-/* 	} */
-	  
-/*     } else { */
-/*       derivs = (deriv_func *) lsoda_derivs; */
-/*       PROTECT(odesolve_deriv_func = func); incr_N_Protect(); */
-/*       PROTECT(odesolve_envir = rho);incr_N_Protect(); */
-/*     } */
-  Free(iwork); Free(work);
 
   UNPROTECT(nprotect);
   return ans;
