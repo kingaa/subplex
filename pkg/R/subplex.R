7f25bfd6cbc68fd2822cdc6a69c4916b269396db
Aaron A. King <kingaa@umich.edu>
2009-05-07 14:24:08 +0000

    change interface to more closely resemble that of 'optim'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/subplex@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/subplex.R
+++ pkg/R/subplex.R
@@ -1,10 +1,20 @@
-subplex <- function (par, fn, tol = .Machine$double.eps, maxnfe = 10000, scale = 1, hessian = FALSE, ...) {
-  .Call("call_subplex",
+subplex <- function (par, fn, control = list(), hessian = FALSE, ...) {
+## set default control parameters
+  con <- list(
+              reltol = .Machine$double.eps,
+              maxit = 10000,
+              parscale = rep.int(1,length(par))
+              )
+  namc <- names(control)[names(control)%in%names(con)]
+  con[namc] <- control[namc]
+  print(con)
+  .Call(
+        "call_subplex",
         par,
         match.fun(fn),
-        tol,
-        maxnfe,
-        scale,
+        tol=con$reltol,
+        maxnfe=con$maxit,
+        scale=con$parscale,
         hessian,
         environment(fn),
         pairlist(...)
