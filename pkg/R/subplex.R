a5622fd668d4c2d7d832408a0e9f42546b82d873
Aaron A. King <kingaa@umich.edu>
2009-05-07 14:10:05 +0000

    bug fix
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/subplex@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/subplex.R
+++ pkg/R/subplex.R
@@ -1,6 +1,13 @@
 subplex <- function (par, fn, tol = sqrt(.Machine$double.eps), maxnfe = 10000, scale = 1) {
+  if (!is.numeric(par)) {
+    nm <- names(par)
+    par <- as.numeric(par)
+    names(par) <- nm
+  }
+  if (length(scale)>1 && length(scale)!=length(par))
+    stop("'scale' misspecified: either specify a single scale or one for each component of 'par'")
   .Call("call_subplex",
-        as.double(par),
+        par,
         fn,
         as.double(tol),
         as.integer(maxnfe),
