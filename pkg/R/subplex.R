5a74835df6e4b63d11b208c6ab178f664b2e4790
Aaron A. King <kingaa@umich.edu>
2009-05-07 14:15:00 +0000

    add some error handling
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/subplex@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/subplex.R
+++ pkg/R/subplex.R
@@ -4,6 +4,10 @@ subplex <- function (par, fn, tol = sqrt(.Machine$double.eps), maxnfe = 10000, s
     par <- as.numeric(par)
     names(par) <- nm
   }
+  if (!is.function(fn)) stop("'fn' must be a function")
+  if (!is.numeric(tol) || (length(tol)>1) || (tol <= 0))
+    stop("'tol' must be a positive scalar")
+  if (maxnfe <= 0) stop("'maxnfe' must be a positive integer")
   if (length(scale)>1 && length(scale)!=length(par))
     stop("'scale' misspecified: either specify a single scale or one for each component of 'par'")
   .Call("call_subplex",
