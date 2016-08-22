0310a3e4c467e981fa74eef59d5671e0bd85fd42
Aaron A. King <kingaa@umich.edu>
2016-08-22 17:18:14 -0400

    - remove unneeded BLAS - improve test suite

--- tests/ripple.R
+++ tests/ripple.R
@@ -10,3 +10,14 @@ subplex(par=c(1),fn=ripple,hessian=TRUE)
 subplex(par=c(0.1,3),fn=ripple,hessian=TRUE)
 
 subplex(par=c(0.1,3,2),fn=ripple,hessian=TRUE)
+
+try(subplex(par=c(0.1,3,2),fn=3,hessian=FALSE))
+try(subplex(par=c(0.1,3,2),fn=ripple,control=list(reltol=-100)))
+try(subplex(par=c(0.1,3,2),fn=ripple,control=list(maxit=-100)))
+
+subplex(par=c(0.1,3,2),fn=ripple,control=list(parscale=0.1))
+try(subplex(par=c(0.1,3,2),fn=ripple,control=list(parscale=c(0.1,0.5))))
+
+try(subplex(par=c(0.1,3,2),fn=ripple,control=list(maxit=2)))
+
+try(subplex(par=c(0.1,3,2),fn=ripple,control=list(reltol=1e-3)))
