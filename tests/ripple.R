3fa3ffc17dec3896fff09a932f5f57e95c840e52
Aaron A. King <kingaa@umich.edu>
2016-11-18 17:38:05 -0500

    - modify tests to avoid 64/32-bit incompatibility on windows - add url to original version of Tom Rowan's codes on netlib

--- tests/ripple.R
+++ tests/ripple.R
@@ -5,19 +5,26 @@ ripple <- function (x) {
     1-exp(-r^2)*cos(10*r)^2
 }
 
-subplex(par=c(1),fn=ripple,hessian=TRUE)
+fit <- subplex(par=c(1),fn=ripple,hessian=TRUE)
+stopifnot(all.equal(fit$par,0,tol=1e-5))
 
-subplex(par=c(0.1,3),fn=ripple,hessian=TRUE)
+fit <- subplex(par=c(0.1,3),fn=ripple,hessian=TRUE)
+stopifnot(all.equal(fit$value,0,tol=1e-5))
 
-subplex(par=c(0.1,3,2),fn=ripple,hessian=TRUE)
-
+fit <- subplex(par=c(0.1,3,2),fn=ripple,hessian=TRUE)
+stopifnot(all.equal(fit$par,c(0.45932,1.10399,0.34408),tol=1e-4))
+    
 try(subplex(par=c(0.1,3,2),fn=3,hessian=FALSE))
 try(subplex(par=c(0.1,3,2),fn=ripple,control=list(reltol=-100)))
 try(subplex(par=c(0.1,3,2),fn=ripple,control=list(maxit=-100)))
 
-subplex(par=c(0.1,3,2),fn=ripple,control=list(parscale=0.1))
+fit <- subplex(par=c(0.1,3,2),fn=ripple,control=list(parscale=0.1))
+stopifnot(all.equal(fit$value,1,tol=1e-4))
+
 try(subplex(par=c(0.1,3,2),fn=ripple,control=list(parscale=c(0.1,0.5))))
 
-try(subplex(par=c(0.1,3,2),fn=ripple,control=list(maxit=2)))
+fit <- subplex(par=c(0.1,3,2),fn=ripple,control=list(maxit=2))
+stopifnot(fit$conv==-1)
 
-try(subplex(par=c(0.1,3,2),fn=ripple,control=list(reltol=1e-3)))
+fit <- subplex(par=c(0.1,3,2),fn=ripple,control=list(reltol=1e-3))
+stopifnot(all.equal(fit$value,0.791,tol=1e-3))
--- tests/rosen.R
+++ tests/rosen.R
@@ -7,14 +7,16 @@ rosen <- function (x) {
     100*(x2-x1*x1)^2+(1-x1)^2
 }
 
-subplex(par=c(11,-33),fn=rosen)
+fit <- subplex(par=c(11,-33),fn=rosen)
+stopifnot(all.equal(fit$par,c(1,1),tol=1e-4))
 
 rosen2 <- function (x) {
     X <- matrix(x,ncol=2) 
     sum(apply(X,1,rosen))
 }
 
-subplex(par=c(-33,11,14,9,0,12),fn=rosen2,control=list(maxit=30000))
+fit <- subplex(par=c(-33,11,14,9,0,12),fn=rosen2,control=list(maxit=30000))
+stopifnot(all.equal(fit$par,c(1,1,1,1,1,1),tol=1e-4))
 
 ## Rosenbrock Banana function (using names)
 rosen <- function (x, g = 0, h = 0) {
@@ -23,5 +25,7 @@ rosen <- function (x, g = 0, h = 0) {
     100*(x2-x1*x1)^2+(1-x1)^2+g
 }
 
-subplex(par=c(b=11,a=-33),fn=rosen,h=22,
-        control=list(abstol=1e-9,parscale=5),hessian=TRUE)
+fit <- subplex(par=c(b=11,a=-33),fn=rosen,h=22,
+               control=list(abstol=1e-9,parscale=5),hessian=TRUE)
+stopifnot(all.equal(fit$par,c(b=23,a=1),tol=1e-5))
+stopifnot(all.equal(as.numeric(fit$hessian),c(200,-400,-400,802),tol=1e-5))
