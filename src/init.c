f3dd83ac5adaac041e7851f45c501f325b1b0c30
Aaron A. King <kingaa@umich.edu>
2017-07-17 16:59:37 -0400

    register C Callable routines

---
+++ src/init.c
@@ -0,0 +1,16 @@
+#include <R.h>
+#include <Rdefines.h>
+#include <R_ext/Rdynload.h>
+
+extern SEXP call_subplex (SEXP x, SEXP f, SEXP tol, SEXP maxnfe, SEXP scale, SEXP hessian, SEXP rho, SEXP args);
+
+static const R_CallMethodDef callMethods[] = {
+  {"call_subplex", (DL_FUNC) &call_subplex, 8},
+  {NULL, NULL, 0}
+};
+
+void R_init_subplex (DllInfo *info) {
+  // Register routines
+  R_registerRoutines(info,NULL,callMethods,NULL,NULL);
+  R_useDynamicSymbols(info,TRUE);
+}
