1e43a753283be5a9596cb1c5b5a24a5fb264de42
Aaron A. King <kingaa@umich.edu>
2018-02-11 10:25:26 -0500

    update test

--- tests/ripple.R
+++ tests/ripple.R
@@ -18,6 +18,9 @@ stopifnot(all.equal(fit$par,0,tol=1e-5))
 fit <- subplex(par=c(0.1,3),fn=ripple,hessian=TRUE)
 stopifnot(all.equal(fit$value,0,tol=1e-5))
 
+fit <- subplex(par=c(0.1,3),fn=ripple,hessian=TRUE,
+               control=list(parscale=c(0.01,0.001)))
+
 fit <- subplex(par=c(0.1,3,2),fn=ripple,hessian=TRUE)
 stopifnot(all.equal(fit$par,c(0.45932,1.10399,0.34408),tol=1e-4))
 print(fit$message)
@@ -30,6 +33,7 @@ print(fit$message)
 try(subplex(par=c(0.1,3,2),fn=3,hessian=FALSE))
 try(subplex(par=c(0.1,3,2),fn=ripple,control=list(reltol=-100)))
 try(subplex(par=c(0.1,3,2),fn=ripple,control=list(maxit=-100)))
+try(subplex(par=numeric(0),fn=ripple))
 
 fit <- subplex(par=c(0.1,3,2),fn=ripple,control=list(parscale=0.1))
 stopifnot(all.equal(fit$value,1,tol=1e-4))
